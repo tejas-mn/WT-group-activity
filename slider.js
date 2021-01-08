e10dc5c53ea8fc62665e1a1e6ad7ddba5234c02c
tejas-mn <68629215+tejas-mn@users.noreply.github.com>
2021-01-08 21:24:49 +0530

    Update slider.js

--- slider.js
+++ slider.js
@@ -3,10 +3,10 @@ var BackBtn = document.getElementById("BackBtn");
 var NextBtn = document.getElementById("NextBtn");
 
 var slideimages = new Array(
-    "images/1.jpg",
-    "images/2.jpg",
-    "images/3.jpg",
-    "images/4.jpg",
+    "images/1.JPG",
+    "images/2.JPG",
+    "images/3.JPG",
+    "images/4.JPG",
 );
 
 var i=0;
